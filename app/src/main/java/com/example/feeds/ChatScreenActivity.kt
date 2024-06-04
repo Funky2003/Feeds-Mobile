a9a92714678e7577a956f211f52ea713ebfc544d
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-06-04 18:08:20 +0000

    Code update: Implemented the realtime bi-directional conversation...

--- app/src/main/java/com/example/feeds/ChatScreenActivity.kt
+++ app/src/main/java/com/example/feeds/ChatScreenActivity.kt
@@ -4,28 +4,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import com.example.feeds.adapters.ChatAdapter
-import com.example.feeds.models.MessageModel
-import com.example.feeds.network.Connectivity
 import com.example.feeds.supabase.SupaBase
-import com.example.feeds.supabase.supabase
-import io.github.jan.supabase.realtime.PostgresAction
-import io.github.jan.supabase.realtime.channel
-import io.github.jan.supabase.realtime.decodeRecord
-import io.github.jan.supabase.realtime.postgresChangeFlow
-import io.github.jan.supabase.realtime.postgresListDataFlow
-import io.github.jan.supabase.realtime.realtime
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class ChatScreen : AppCompatActivity() {
     private val supaBase = SupaBase()
@@ -64,7 +48,6 @@ class ChatScreen : AppCompatActivity() {
         nUsername.text = name
         profileAvatar.setImageResource(profile)
     }
-
     fun sendMessage(view: View) {
         supaBase.sendMessage(this@ChatScreen)
     }
